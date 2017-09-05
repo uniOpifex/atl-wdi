1ea9e35a23238749a6f93a44edfc6c99a283681f
uniOpifex <njhunit@gmail.com>
2017-09-05 13:54:11 -0400

    js homework updates

---
+++ homework/javaScript/function-drills/hw.js
@@ -0,0 +1,192 @@
+// General Assembly, WDI (Web Development Immersive) Remote, Cohort 02 (R2D2)
+// Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
+// See LICENSE for details.
+"use strict";
+/// DO NOT EDIT ABOVE THIS LINE ///
+
+////// WRITE A FUNCTION THAT TAKES ___ AS INPUT AND RETURNS ___ AS OUTPUT //////
+
+// #1
+// Input: an array of numbers
+// Output: the sum of the numbers that were passed in
+// Edge Case: If the array is empty, return 0
+var sumOfNums = function(numsArray){
+  var sum = 0;
+  for (var i = 0; i < numsArray.length; i++){
+    sum += numsArray[i];
+  }
+  return sum;
+};
+
+// #2
+// Input: an array of numbers
+// Output: an array of the numbers from the first array that are strictly
+//         greater (i.e. greater than but not equal to) than 10
+var numsGreaterThanTen = function(numsArray){
+  var results = [];
+  numsArray.forEach(function(entry) {
+    if (entry > 10){
+      results.push(entry);
+    }
+  });
+  return results;
+};
+
+// #3
+// Input: an array of numbers
+// Output: `true` if ALL numbers passed in are strictly greater than 10;
+//         `false` otherwise
+// Edge Case: If the input array is empty, the function should return `true`.
+var allGreaterThanTen = function(numsArray){
+  for (var i = 0; i < numsArray.length; i++){
+    if (!(numsArray[i] > 10)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// #4
+// Input: an array of words
+// Output: an array of all words from the first array with five or more letters
+var wordsWithAtLeastFiveLetters = function(words){
+  var fiveLetterWords = [];
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length >= 5){
+      fiveLetterWords.push(words[i]);
+    }
+  }
+  return fiveLetterWords;
+};
+
+// #5
+// Input: an array of words
+// Output: `true` if ALL words start with the letter 'a' (case-insensitive),
+//          `false` otherwise
+// Edge Case: If the array is empty, the function should return `true`.
+var allStartingWithA = function(words) {
+  for (var i = 0; i < words.length; i++) {
+    if(!(words[i].slice(0, 1) === 'a' || words[i].slice(0, 1) === 'A')){
+      return false;
+    }
+  }
+  return true;
+};
+
+// #6
+// Input: an array of words
+// Output: `true` if there are ANY words that start with the letter 'b'
+//          (case-insensitive), `false` otherwise
+var anyStartingWithB = function(words){
+  if(words.length === 0)
+    return false;
+  for(let i = 0; i < words.length; i++){
+    if('b' === words[i][0].toLowerCase())
+      return true;
+  }
+  return false;
+};
+
+// #7
+// Input: a single word and a number (`n`)
+// Output: `true` if the word has at least some number (`n`) of vowels,
+//          `false` otherwise
+//    Assume that vowels are 'a', 'e', 'i', 'o', and 'u' (NOT 'y')
+// Edge Case: If `n` is less than zero, return `null`.
+var hasAtLeastNVowels = function(word, n){
+  if (n < 0) {
+    return null;
+  }
+  var count = 0;
+  for (var i = 0; i < word.length; i++) {
+    if((word.charAt(i) === 'a' || word.charAt(i) === 'A') || word.charAt(i) === 'e' || word.charAt(i) === 'E' || word.charAt(i) === 'i' || word.charAt(i) === 'I' || word.charAt(i) === 'o' || word.charAt(i) === 'O' || word.charAt(i) === 'u' || word.charAt(i) === 'U') {
+      count++;
+    }
+  }
+  return count >= n;
+};
+
+// #8
+// Input: an array of words
+// Output: an array of words from the original array that have at least two
+//          vowels
+var wordsWithAtLeastTwoVowels = function(words){
+var results = [];
+  for (var i = 0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      results.push(words[i]);
+    }
+  }
+  return results;
+};
+
+// #9
+// Input: an array of words
+// Output: `true` if ALL words have two or more vowels, `false` otherwise
+// Edge Case: If the array is empty, the function should return `true`.
+var allHaveAtLeastTwoVowels = function(words){
+  for (var i = 0; i < words.length; i++) {
+    if (!hasAtLeastNVowels(words[i], 2)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// #10
+// Input: an array of words
+// Output: `true` if there are ANY words have two or more vowels,
+//          `false` otherwise.
+var anyHaveAtLeastTwoVowels = function(words){
+  for (var i =0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// #11
+// Input: an array of words
+// Output: `true` if NONE of the words have two or more vowels,
+//          `false` otherwise
+// Edge Case: If the array is empty, the function should return `true`.
+var noneHaveTwoOrMoreVowels = function(words){
+  for (var i = 0; i < words.length; i++) {
+    if (hasAtLeastNVowels(words[i], 2)) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+// #12
+// Input: an array of words
+// Output: an object ({}) where each word in the array is a key, and the value
+//          tied to that key is the length of the word.
+// e.g. given ['cat', 'horse', 'elephant'],
+//      return { cat: 3, horse: 5, elephant: 8}
+var buildObjectFromWords = function(words){
+  var result = {};
+  for (var i = 0; i < words.length; i++) {
+    result[words[i]] = words[i].length;
+  }
+  return result;
+};
+
+
+/// DO NOT EDIT BELOW THIS LINE ///
+module.exports = {
+  sumOfNums: sumOfNums,
+  numsGreaterThanTen: numsGreaterThanTen,
+  allGreaterThanTen: allGreaterThanTen,
+  wordsWithAtLeastFiveLetters: wordsWithAtLeastFiveLetters,
+  allStartingWithA: allStartingWithA,
+  anyStartingWithB: anyStartingWithB,
+  hasAtLeastNVowels: hasAtLeastNVowels,
+  wordsWithAtLeastTwoVowels: wordsWithAtLeastTwoVowels,
+  allHaveAtLeastTwoVowels: allHaveAtLeastTwoVowels,
+  anyHaveAtLeastTwoVowels: anyHaveAtLeastTwoVowels,
+  noneHaveTwoOrMoreVowels: noneHaveTwoOrMoreVowels,
+  buildObjectFromWords: buildObjectFromWords
+}
--- morning_exercises/javaScript/js-practice/classwork.js
+++ morning_exercises/javaScript/js-practice/classwork.js
@@ -10,52 +10,75 @@
 
 // 1. true || false 
 var resOne;
-// Replace This Comment With Your Code
+resOne = true;
 
 // 2. null || ''
 var resTwo;
-// Replace This Comment With Your Code
+reTwo = true;
 
 // 3. 'false' && true
 var resThree;
-// Replace This Comment With Your Code
+resThree = true;
 
 // 4. !!0
 var resFour;
-// Replace This Comment With Your Code
+resFour = true;
 
 // 5. !null && !undefined
 var resFive;
-// Replace This Comment With Your Code
+resFive = true;
 
 /// Flow Control ///
 
 // 6. Write a `while` loop that increases `resSix` by 23 until `resSix` is five
 //    digits long. (Hint: take a look at the .toString() method)
 var resSix;
-// Replace This Comment With Your Code
+reSix = 0;
+while(resSix.toString().length < 5){
+  resSix += 23;
+  
+}
+
 
 // 7. Write a `while` loop that increases `resSeven` by 12 when it's strictly
 //    below 95, decreases it by 7 when it's strictly above 105, and stops as
 //    soon as `resSeven` equals or falls between those two values.
 var resSeven;
-// Replace This Comment With Your Code
+resSeven = 0;
+
+resSeven = 
+while (resSeven >=94 && resSeven <=105) {
+  return resSeven;
+  break;
+}
+while (resSeven <= 95 ){
+  resSeven += 12
+}
+while (reseSeven >= 105){
+  resSeven -= 7;
+}
+;
 
 // 8. Write a `for` loop that counts from 0 (inclusive) to 1857 (exclusive) and
 //    calculates the sum of all of those numbers, storing the result in the
 //    variable `resEight`.
 var resEight;
-// Replace This Comment With Your Code
+resEight = 0;
+for (i = 0; i < 1857; i++){
+  resEight = resEight + i;
+}
 
 // 9. Using a `for` loop, calculate the sum of all multiples of 7 between 0
 //    (inclusive) and 1000 (exclusive); store the result in the variable
 //    `resNine`.
 var resNine;
-// Replace This Comment With Your Code
+resNine = 0;
 
 // 10. For numbers 1-10, calculate n^n and then store the sum of all of these values in the variable `resTen`.
 var resTen;
-// Replace This Comment With Your Code
+for (i = 0; i < 10; i++){
+  resTen += Math.pow(i, i);
+}
 
 /// DO NOT EDIT BELOW THIS LINE ///
 module.exports = {
