ca6e3c9602d70b8467f5245d3f42a108cb554ddd
uniOpifex <njhunit@gmail.com>
2017-09-20 14:17:17 -0400

    -

---
+++ homework/pizza-express/app.js
@@ -0,0 +1,20 @@
+// app.js
+
+//require express package
+var express = require('express');
+//save an express module as 'app'
+var app     = express();
+// assigning 3000 as our port
+var port    = 3000;
+
+app.get('/topping/:type', function(req, res, next) {
+  
+      res.send("/type Pizza!");
+  });
+
+// tells the server to listen for requests on port 3000
+app.listen(port, function(){
+  console.log("==========================")
+  console.log('LISTENING ON PORT ' + port);
+  console.log("==========================")
+});
