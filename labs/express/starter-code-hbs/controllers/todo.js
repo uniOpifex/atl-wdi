87728ac9c7ac53a7050245e28c84a1972ceba68c
uniOpifex <njhunit@gmail.com>
2017-09-20 15:20:24 -0400

    updated TODO New  controller

--- labs/express/starter-code-hbs/controllers/todo.js
+++ labs/express/starter-code-hbs/controllers/todo.js
@@ -10,25 +10,29 @@ router.get('/', function(req,res) {
     });
 });
 
+router.get('/new', (req, res) =>{
+    res.render('todos/new')
+})
+
 /*
 SHOW TODS
 */
 
 router.get('/:id', (req, res) => {
         const id = req.params.id;
-        const todo = data.seededTodos.Todos[id];
+        const todo = data.seededTodos[id];
         if (!todo){
         res.render('todos/show', {
             //todo,
             error: "no to-do found with this ID"
         })
     } else {
-        res.render('todos/show', {
-            //todo,
-            error: "no to-do found with this ID"
-        })
+        res.render('todos/show', {todo})
     }
 })
+
+
+
 
 
 module.exports = router;
