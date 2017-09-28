028da27a6cdd34645f235fe8291d88b39900d490
uniOpifex <njhunit@gmail.com>
2017-09-27 22:57:58 -0400

    updated promise-burger

--- labs/javaScript/promise-burger/hw.js
+++ labs/javaScript/promise-burger/hw.js
@@ -7,6 +7,9 @@ const kitchen = require('./kitchen');
 // Call this method and console.log the data that comes back when the Promise is resolved.
 
 //CODE FOR QUESTION 1 HERE
+kitchen.readMenu()
+  .then(console.log)
+  .catch(console.log);
 
 
 // Order Some Food
@@ -20,16 +23,19 @@ const kitchen = require('./kitchen');
 
 // CODE FOR QUESTION 2 HERE
 
-var myorder = (kitchen.order('FAT ELVIS'));
-console.log((myorder));
+kitchen.order('Big Bad Voodoo Patty')
+  .then(console.log)
+  .catch(console.log);
 
 // 3. Try and use the same method to order a `quesadilla`.  
 //  Since Promise Burger only serves burgers, this will throw an error.  
 //  Make sure that your Promise can be dealt with when it is rejected.
 
 // CODE FOR QUESTION 3 HERE
+kitchen.order('quesadilla')
+  .then(console.log)
+  .catch(console.log);
 
-//
 
 
 // Add A Burger To The Menu
@@ -46,10 +52,13 @@ const newBurger = {
   "price": 28.95,
   "description": ` It becomes difficult to even describe this, the most preposterous of our Bypass Burgers. All you really need to know is that we use three burger patties, three fried eggs, fourteen slices of American cheese, and ten slices of bacon, all packed between two grilled cheese sandwiches. Figuring out how to add condiments is completely up to you. It’s served in a big bowl of french fries and tater tots covered, in lots of our Cheesy-Cheese Goo. Enjoy!`
 }
-kitchen.addToMenu(newBurger);
-const newKitchen = kitchen.readMenu();
-console.log(newKitchen);
+
 // CODE FOR QUESTION 4 HERE
+kitchen.addToMenu(newBurger)
+  .then(console.log)
+  .catch(console.log)
+    .then(console.log(kitchen.readMenu()));
+
 
 // 5. Validate that the new item has been added to the menu by calling `readMenu` again.  
 // We only want to call `readMenu` after we get a response that is successful. 
